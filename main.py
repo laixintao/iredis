6f31b52460e0c69816d260f8d99405e72371920a
laixintao <laixintao1995@163.com>
2019-06-28 14:15:29 +0800

    test install prompt_toolkit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from prompt_toolkit import prompt
 
-if __name__ == '__main__':
-    answer = prompt('Give me some input: ')
-    print('You said: %s' % answer)
+text = prompt('Give me some input: ')
+print('You said: %s' % text)
