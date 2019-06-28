8a374f3001a12fedc25122fe0badb3ef2cba5aaa
laixintao <laixintao1995@163.com>
2019-06-28 15:50:06 +0800

    history support

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from prompt_toolkit import prompt
+# -*- coding: utf-8 -*-
+import os
+from pathlib import Path
+from prompt_toolkit import PromptSession
+from prompt_toolkit.history import FileHistory
 
-text = prompt('Give me some input: ')
-print('You said: %s' % text)
+HISTORY_FILE = Path(os.path.expanduser("~")) / ".rdcli_history"
+
+# Create history file if not exists.
+if not os.path.exists(HISTORY_FILE):
+    f = open(HISTORY_FILE, "w+")
+    f.close()
+session = PromptSession(history=FileHistory(HISTORY_FILE))
+
+
+while True:
+    session.prompt()
