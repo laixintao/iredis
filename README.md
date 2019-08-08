58c9806d9ecce8967bf0c907bae8e42232b17024
laixintao <nanshi.lxt@alipay.com>
2019-08-08 20:20:50 +0800

    handle Ctrl-C to clear current prompt

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rdcli
 redis-cli replacement.
+
+## Features
+
+- <kbd>Ctrl</kbd> + <kbd>C</kbd> to clear cureent line, won't exit redis-cli. Use <kbd>Ctrl</kbd> + <kbd>D</kbd>  
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 import logging
 from pathlib import Path
 
@@ -20,11 +21,11 @@ HISTORY_FILE = Path(os.path.expanduser("~")) / ".rdcli_history"
 
 
 class Client:
-    def __init__(self, host, port, db):
-        self.host = host
-        self.port = port
-        self.db = db
-        self._redis_client = redis.StrictRedis(host, port, db)
+    def __init__(self, h, p, n):
+        self.host = h
+        self.port = p
+        self.db = n
+        self._redis_client = redis.StrictRedis(self.host, self.port, self.db)
 
     def __str__(self):
         return f"{self.host}:{self.port}[{self.db}]> "
@@ -36,13 +37,20 @@ class Client:
 def repl(client, session):
     while True:
         logger.debug("REPL waiting for command...")
-        command = session.prompt(str(client))
+        try:
+            command = session.prompt(str(client))
+        except KeyboardInterrupt:
+            logger.warning("KeyboardInterrupt!")
+            continue
+        except EOFError:
+            print("Goodbye!")
+            sys.exit()
         logger.info(f"Command: {command}")
 
         # blank input
         if not command:
             continue
-        
+
         try:
             answer = client.send_command(command)
 
@@ -54,12 +62,21 @@ def repl(client, session):
             print(answer)
 
 
+# command line entry here...
 @click.command()
+@click.pass_context
 @click.option("-h", help="Server hostname", default="127.0.0.1")
 @click.option("-p", help="Server port", default="6379")
 @click.option("-n", help="Database number.", default="0")
-def rdcli(h, p, n):
+def rdcli_entry(ctx, h, p, n):
     logger.info(f"rdcli start, host={h}, port={p}, db={n}.")
+    return ctx
+
+
+if __name__ == "__main__":
+    # invoke in non-standalone mode to gather args
+    ctx = rdcli_entry.main(standalone_mode=False)
+
     # Create history file if not exists.
     if not os.path.exists(HISTORY_FILE):
         logger.info(f"History file {HISTORY_FILE} not exists, create now...")
@@ -68,13 +85,5 @@ def rdcli(h, p, n):
 
     session = PromptSession(history=FileHistory(HISTORY_FILE))
 
-    client = Client(h, p, n)
+    client = Client(**ctx.params)
     repl(client, session)
-
-
-if __name__ == "__main__":
-
-    try:
-        rdcli()
-    except click.exceptions.Abort as e:
-        logger.warn(e)
