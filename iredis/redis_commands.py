5e62712fcb1ba75e89e3418be5a4e17d97178ede
laixintao <laixintao1995@163.com>
2019-08-11 01:39:55 +0800

    add multi key support; add timer log

--- iredis/redis_commands.py
+++ iredis/redis_commands.py
@@ -1,4 +1,5 @@
 REDIS_COMMANDS = r"""
-(\s*  (?P<command_key>(HGETALL|GET))   \s+   (?P<key>[0-9.]+)   \s*) |
-(\s*  (?P<command_key_value>(SET|KSET))   \s+   (?P<key>[0-9.]+)   \s+   (?P<value>[0-9.]+)   \s*) 
+(\s*  (?P<command_key>(HGETALL|GET))      \s+   (?P<key>[0-9.]+)                                            \s*)|
+(\s*  (?P<command_key_fields>(HDEL))      \s+   (?P<key>[0-9.]+) \s+  ((?P<key>[0-9.]+)(\s+|$))*            )|
+(\s*  (?P<command_key_value>(SET|KSET))   \s+   (?P<key>[0-9.]+)   \s+   (?P<value>[0-9.]+)                 \s*) 
 """
