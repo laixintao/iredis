7e095f8d0dec3149f999296c5ceb80c7c5accc2a
laixintao <nanshi.lxt@alipay.com>
2019-08-11 13:14:06 +0800

    add double quote match

--- iredis/redis_commands.py
+++ iredis/redis_commands.py
@@ -1,5 +1,13 @@
-REDIS_COMMANDS = r"""
-(\s*  (?P<command_key>(HGETALL|GET))      \s+   (?P<key>[0-9.]+)                                            \s*)|
-(\s*  (?P<command_key_fields>(HDEL))      \s+   (?P<key>[0-9.]+) \s+  ((?P<key>[0-9.]+)(\s+|$))*            )|
-(\s*  (?P<command_key_value>(SET|KSET))   \s+   (?P<key>[0-9.]+)   \s+   (?P<value>[0-9.]+)                 \s*) 
+VALID_TOKEN = r"""(
+("([^"]|\\")*?")     |# with quotes
+([^\s"]+)             # without quotes
+)"""
+KEY = f"""(?P<key>{VALID_TOKEN})"""
+VALUE = f"""(?P<value>{VALID_TOKEN})"""
+FIELDS = f"""(?P<fields>{VALID_TOKEN}(\s+{VALID_TOKEN})*)"""
+
+REDIS_COMMANDS = f"""
+(\s*  (?P<command_key>(HGETALL|GET))      \s+  {KEY}                                    \s*)|
+(\s*  (?P<command_key_fields>(HDEL))      \s+  {KEY}  \s+ {FIELDS}                      \s*)|
+(\s*  (?P<command_key_value>(SET|KSET|HAAA))   \s+   {KEY}   \s+   {VALUE}                \s*) 
 """
