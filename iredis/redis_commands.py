c4701a7a82a2ca9dc5cc82631fcf8df8a6d25874
laixintao <laixintao1995@163.com>
2019-08-12 22:24:03 +0800

    command slots works!

--- iredis/redis_commands.py
+++ iredis/redis_commands.py
@@ -1,3 +1,6 @@
+import csv
+import copy
+
 VALID_TOKEN = r"""(
 ("([^"]|\\")*?")     |# with quotes
 ([^\s"]+)             # without quotes
@@ -9,8 +12,25 @@ KEY = f"""(?P<key>{VALID_TOKEN})"""
 VALUE = f"""(?P<value>{VALID_TOKEN})"""
 FIELDS = f"""(?P<fields>{VALID_TOKEN}(\s+{VALID_TOKEN})*)"""
 
+
+# FIXME path
+t = {}
+with open("command_syntax.csv") as command_syntax:
+    csvreader = csv.reader(command_syntax)
+    for line in csvreader:
+        group, command, syntax = line
+        t.setdefault(syntax, []).append(command)
+
+original_commands = copy.deepcopy(t)
+
+for syntax in t.keys():
+    commands = t[syntax]
+    re_commands = [command.replace(" ","\s+") for command in commands]
+    t[syntax] = "|".join(re_commands)
+
+
 REDIS_COMMANDS = f"""
-(\s*  (?P<command_slots>({command_slots}))        \s+ {SLOTS}                                    \s*)|
+(\s*  (?P<command_slots>({t['command_slots']}))        \s+ {SLOTS}                                    \s*)|
 
 (\s*  (?P<command_key>(HGETALL|GET))      \s+  {KEY}                                    \s*)|
 (\s*  (?P<command_key_fields>(HDEL))      \s+  {KEY}  \s+ {FIELDS}                      \s*)|
