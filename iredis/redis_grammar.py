6972ac3ce680c381cb7016aa36c4a342d96e1a9a
laixintao <laixintao1995@163.com>
2019-08-12 22:55:33 +0800

    refactor: use a single file to load csv

--- iredis/redis_grammar.py
+++ iredis/redis_grammar.py
@@ -1,7 +1,5 @@
-import csv
-import copy
-
 from prompt_toolkit.contrib.regular_languages.compiler import compile
+from .commands_csv_loader import t
 
 VALID_TOKEN = r"""(
 ("([^"]|\\")*?")     |# with quotes
@@ -13,24 +11,6 @@ SLOTS = f"""(?P<slots>{VALID_SLOT}(\s+{VALID_SLOT})*)"""
 KEY = f"""(?P<key>{VALID_TOKEN})"""
 VALUE = f"""(?P<value>{VALID_TOKEN})"""
 FIELDS = f"""(?P<fields>{VALID_TOKEN}(\s+{VALID_TOKEN})*)"""
-
-
-# FIXME path
-t = {}
-with open("command_syntax.csv") as command_syntax:
-    csvreader = csv.reader(command_syntax)
-    for line in csvreader:
-        group, command, syntax = line
-        t.setdefault(syntax, []).append(command)
-
-original_commands = copy.deepcopy(t)
-
-for syntax in t.keys():
-    commands = t[syntax]
-    lower_commands = [command.lower() for command in commands]
-    commands += lower_commands
-    re_commands = [command.replace(" ", "\s+") for command in commands]
-    t[syntax] = "|".join(re_commands)
 
 
 REDIS_COMMANDS = f"""
