2063d90b354e1014548b8afa935dbc849a123019
laixintao <laixintao1995@163.com>
2019-08-12 21:33:11 +0800

    sort parse result, keep results same.

--- scripts/download_redis_commands.py
+++ scripts/download_redis_commands.py
@@ -26,17 +26,22 @@ eprint("Start prase page...")
 html = etree.HTML(page)
 commands = html.xpath("//div[@class='container']/ul/li")
 stdout_writer.writerow(["Group", "Command", "Args", "Summary", "Redis.io link"])
+command_rows = []
+# parse page
 for command in commands:
     group = command.attrib['data-group']
     command_name = command.xpath("./a/span[@class='command']/text()")[0].strip()
     command_args = command.xpath("./a/span[@class='command']/span[@class='args']/text()")[0].strip()
     command_summary = command.xpath("./a/span[@class='summary']/text()")[0].strip()
     command_link = "https://redis.io" + command.xpath("./a/@href")[0].strip()
-    stdout_writer.writerow([
+    command_rows.append([
         group,
         command_name,
         " ".join(command_args.split()),
         command_summary,
         command_link,
     ])
+# write to stdout
+for row in sorted(command_rows):
+    stdout_writer.writerow(row)
 eprint("Down.")
