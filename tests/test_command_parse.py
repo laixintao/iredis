bf121f34dce6a5bfb2b8a3a16e6de6526e151d0c
laixintao <laixintao1995@163.com>
2019-08-13 21:14:10 +0800

    refactor test

--- tests/test_command_parse.py
+++ tests/test_command_parse.py
@@ -1,123 +1,107 @@
-from iredis.redis_grammar import redis_grammar
-
-
-def judge(command, expect):
-    m = redis_grammar.match(command)
-
-    # not match
-    if not expect:
-        assert m is None
-        return
-
-    variables = m.variables()
-    for expect_token, expect_value in expect.items():
-        assert variables.get(expect_token) == expect_value
-
-
-def test_command_cluster_addslots():
-    judge("cluster addslots 1", {"command_slots": "cluster addslots", "slots": "1"})
-    judge("CLUSTER ADDSLOTS 1", {"command_slots": "CLUSTER ADDSLOTS", "slots": "1"})
-    judge(
+def test_command_cluster_addslots(judge_command):
+    judge_command("cluster addslots 1", {"command_slots": "cluster addslots", "slots": "1"})
+    judge_command("CLUSTER ADDSLOTS 1", {"command_slots": "CLUSTER ADDSLOTS", "slots": "1"})
+    judge_command(
         "cluster addslots 1 2 3 4",
         {"command_slots": "cluster addslots", "slots": "1 2 3 4"},
     )
-    judge("cluster addslots 1 a", None)
-    judge("cluster addslots a", None)
-    judge("cluster addslots a 4", None)
-    judge("cluster addslots abc", None)
+    judge_command("cluster addslots 1 a", None)
+    judge_command("cluster addslots a", None)
+    judge_command("cluster addslots a 4", None)
+    judge_command("cluster addslots abc", None)
 
 
-def test_command_cluster_count_failure_reports():
-    judge(
+def test_command_cluster_count_failure_reports(judge_command):
+    judge_command(
         "cluster count-failure-reports 1",
         {"command_node": "cluster count-failure-reports", "node": "1"},
     )
-    judge(
+    judge_command(
         "CLUSTER COUNT-FAILURE-REPORTS 1",
         {"command_node": "CLUSTER COUNT-FAILURE-REPORTS", "node": "1"},
     )
-    judge("cluster count-failure-reports 1 2 3 4", None)
-    judge("cluster count-failure-reports 1 a", None)
-    judge("cluster count-failure-reports a", None)
-    judge("cluster count-failure-reports a 2", None)
-    judge("cluster count-failure-reports abc", None)
+    judge_command("cluster count-failure-reports 1 2 3 4", None)
+    judge_command("cluster count-failure-reports 1 a", None)
+    judge_command("cluster count-failure-reports a", None)
+    judge_command("cluster count-failure-reports a 2", None)
+    judge_command("cluster count-failure-reports abc", None)
 
 
-def test_command_cluster_countkeysinslot():
-    judge(
+def test_command_cluster_countkeysinslot(judge_command):
+    judge_command(
         "cluster countkeysinslot 1",
         {"command_slot": "cluster countkeysinslot", "slot": "1"},
     )
-    judge(
+    judge_command(
         "CLUSTER COUNTKEYSINSLOT 1",
         {"command_slot": "CLUSTER COUNTKEYSINSLOT", "slot": "1"},
     )
-    judge("cluster countkeysinslot 1 2 3 4", None)
-    judge("cluster countkeysinslot 1 a", None)
-    judge("cluster countkeysinslot a", None)
-    judge("cluster countkeysinslot a 4", None)
-    judge("cluster countkeysinslot abc", None)
+    judge_command("cluster countkeysinslot 1 2 3 4", None)
+    judge_command("cluster countkeysinslot 1 a", None)
+    judge_command("cluster countkeysinslot a", None)
+    judge_command("cluster countkeysinslot a 4", None)
+    judge_command("cluster countkeysinslot abc", None)
 
 
-def test_command_cluster_delslots():
-    judge("cluster delslots 1", {"command_slots": "cluster delslots", "slots": "1"})
-    judge("CLUSTER DELSLOTS 1", {"command_slots": "CLUSTER DELSLOTS", "slots": "1"})
-    judge(
+def test_command_cluster_delslots(judge_command):
+    judge_command("cluster delslots 1", {"command_slots": "cluster delslots", "slots": "1"})
+    judge_command("CLUSTER DELSLOTS 1", {"command_slots": "CLUSTER DELSLOTS", "slots": "1"})
+    judge_command(
         "cluster delslots 1 2 3 4",
         {"command_slots": "cluster delslots", "slots": "1 2 3 4"},
     )
-    judge("cluster delslots 1 a", None)
-    judge("cluster delslots a", None)
-    judge("cluster delslots a 4", None)
-    judge("cluster delslots abc", None)
+    judge_command("cluster delslots 1 a", None)
+    judge_command("cluster delslots a", None)
+    judge_command("cluster delslots a 4", None)
+    judge_command("cluster delslots abc", None)
 
 
-def test_command_cluster_failover():
-    judge(
+def test_command_cluster_failover(judge_command):
+    judge_command(
         "cluster failover force",
         {"command_failoverchoice": "cluster failover", "failoverchoice": "force"},
     )
-    judge(
+    judge_command(
         "cluster failover takeover",
         {"command_failoverchoice": "cluster failover", "failoverchoice": "takeover"},
     )
-    judge(
+    judge_command(
         "CLUSTER FAILOVER FORCE",
         {"command_failoverchoice": "CLUSTER FAILOVER", "failoverchoice": "FORCE"},
     )
-    judge(
+    judge_command(
         "CLUSTER FAILOVER takeover",
         {"command_failoverchoice": "CLUSTER FAILOVER", "failoverchoice": "takeover"},
     )
-    judge(
+    judge_command(
         "CLUSTER FAILOVER TAKEOVER",
         {"command_failoverchoice": "CLUSTER FAILOVER", "failoverchoice": "TAKEOVER"},
     )
 
 
-def test_command_cluster_forget():
-    judge("cluster forget 1", {"command_node": "cluster forget", "node": "1"})
-    judge(
+def test_command_cluster_forget(judge_command):
+    judge_command("cluster forget 1", {"command_node": "cluster forget", "node": "1"})
+    judge_command(
         "CLUSTER COUNT-FAILURE-REPORTS 1",
         {"command_node": "CLUSTER COUNT-FAILURE-REPORTS", "node": "1"},
     )
-    judge("cluster forget 1 2 3 4", None)
-    judge("cluster forget 1 a", None)
-    judge("cluster forget a", None)
-    judge("cluster forget a 2", None)
-    judge("cluster forget abc", None)
+    judge_command("cluster forget 1 2 3 4", None)
+    judge_command("cluster forget 1 a", None)
+    judge_command("cluster forget a", None)
+    judge_command("cluster forget a 2", None)
+    judge_command("cluster forget abc", None)
 
 
-def test_command_cluster_getkeysinslot():
-    judge(
+def test_command_cluster_getkeysinslot(judge_command):
+    judge_command(
         "cluster getkeysinslot 1 1",
         {"command_slot_count": "cluster getkeysinslot", "slot": "1", "count": "1"},
     )
-    judge(
+    judge_command(
         "CLUSTER GETKEYSINSLOT 1 1",
         {"command_slot_count": "CLUSTER GETKEYSINSLOT", "slot": "1", "count": "1"},
     )
-    judge(
+    judge_command(
         "cluster getkeysinslot 1123 1121",
         {
             "command_slot_count": "cluster getkeysinslot",
@@ -125,35 +109,21 @@ def test_command_cluster_getkeysinslot():
             "count": "1121",
         },
     )
-    judge("cluster getkeysinslot 1 2 3 4", None)
-    judge("cluster getkeysinslot 1 a", None)
-    judge("cluster getkeysinslot a", None)
-    judge("cluster getkeysinslot a 4", None)
-    judge("cluster getkeysinslot abc", None)
+    judge_command("cluster getkeysinslot 1 2 3 4", None)
+    judge_command("cluster getkeysinslot 1 a", None)
+    judge_command("cluster getkeysinslot a", None)
+    judge_command("cluster getkeysinslot a 4", None)
+    judge_command("cluster getkeysinslot abc", None)
 
 
-def test_command_cluster_info():
-    judge("cluster info", {"command": "cluster info"})
-    judge("CLUSTER INFO", {"command": "CLUSTER INFO"})
-    judge("CLUSTER INFO 1", None)
-    judge("Acluster info", None)
+def test_command_cluster_info(judge_command):
+    judge_command("cluster info", {"command": "cluster info"})
+    judge_command("CLUSTER INFO", {"command": "CLUSTER INFO"})
+    judge_command("CLUSTER INFO 1", None)
+    judge_command("Acluster info", None)
 
 
-def test_command_cluster_keyslot():
-    judge("cluster keyslot mykey", {"command_key": "cluster keyslot", "key": "mykey"})
-    judge("cluster keyslot MYKEY", {"command_key": "cluster keyslot", "key": "MYKEY"})
-    judge("CLUSTER KEYSLOT MYKEY", {"command_key": "CLUSTER KEYSLOT", "key": "MYKEY"})
-
-
-def test_command_with_key_in_quotes():
-    judge(
-        'cluster keyslot "mykey"', {"command_key": "cluster keyslot", "key": '"mykey"'}
-    )
-    judge(
-        'cluster keyslot "\\"mykey"',
-        {"command_key": "cluster keyslot", "key": '"\\"mykey"'},
-    )
-    judge(
-        'cluster keyslot "mykey "',
-        {"command_key": "cluster keyslot", "key": '"mykey "'},
-    )
+def test_command_cluster_keyslot(judge_command):
+    judge_command("cluster keyslot mykey", {"command_key": "cluster keyslot", "key": "mykey"})
+    judge_command("cluster keyslot MYKEY", {"command_key": "cluster keyslot", "key": "MYKEY"})
+    judge_command("CLUSTER KEYSLOT MYKEY", {"command_key": "CLUSTER KEYSLOT", "key": "MYKEY"})
