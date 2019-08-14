9535aeb23ea86f197f5ff29d8fe9dafe1b77d0ef
laixintao <laixintao1995@163.com>
2019-08-14 19:56:44 +0800

    add command ping

--- tests/test_connection_command_parse.py
+++ tests/test_connection_command_parse.py
@@ -4,3 +4,9 @@ def test_auth(judge_command):
 
 def test_echo(judge_command):
     judge_command("echo hello", {"command_message": "echo", "message": "hello"})
+
+
+def test_ping(judge_command):
+    judge_command("ping hello", {"command_messagex": "ping", "message": "hello"})
+    judge_command("ping", {"command_messagex": "ping", "message": None})
+    judge_command("ping hello world", None)
