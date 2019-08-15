cda81f3190481ee4105160c0271688222ddacfa1
laixintao <laixintao1995@163.com>
2019-08-15 22:27:13 +0800

    add timer counter

--- iredis/utils.py
+++ iredis/utils.py
@@ -5,13 +5,17 @@ import logging
 logger = logging.getLogger(__name__)
 
 _last_timer = time.time()
+_timer_counter = 0
 logger.debug(f"[timer] start on {_last_timer}")
 
 
 def timer(title):
     global _last_timer
-    _last_timer = time.time()
+    global _timer_counter
+
     now = time.time()
     tick = now - _last_timer
+    logger.debug(f"[timer{_timer_counter:2}] {tick:.8f} -> {title}")
+
     _last_timer = now
-    logger.debug(f"[timer] {tick} -> {title}")
+    _timer_counter += 1
