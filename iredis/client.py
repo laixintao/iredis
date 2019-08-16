2adf3d9a492b1849d22e124d382e0d8d7a62b176
laixintao <laixintao1995@163.com>
2019-08-16 20:06:38 +0800

    render function works

--- iredis/client.py
+++ iredis/client.py
@@ -17,7 +17,12 @@ class Client:
     iRedis client, hold a redis-py Client to interact with Redis.
     """
 
-    ANSWER_CALLBACKS = {"HGETALL": renders.render_dict}
+    def reder_funcname_mapping(self):
+        mapping = {}
+        for func_name, func in renders.__dict__.items():
+            if callable(func):
+                mapping[func_name] = func
+        return mapping
 
     def __init__(self, h, p, n):
         self.host = h
@@ -27,7 +32,8 @@ class Client:
             self.host, self.port, self.db, decode_responses=True
         )
         self.connection = Connection()
-        self.answer_callbacks = {}
+        self.answer_callbacks = {"KEYS": "render_int"}
+        self.callbacks = self.reder_funcname_mapping()
 
     def __str__(self):
         return f"{self.host}:{self.port}[{self.db}]"
@@ -49,12 +55,20 @@ class Client:
         "Parses a response from the Redis server"
         try:
             response = connection.read_response()
-        except ResponseError:
-            # only print response error
-            raise
-        # TODO
-        # resp callback
-        return response
+        except ResponseError as e :
+            rendered = str(e)
+        else:
+            # FIXME command name can not only split by " "
+            # command name include " "
+            # for key in variables:
+            #   key.starts_with command_*  then it is command
+            if command_name in self.answer_callbacks:
+                callback_name = self.answer_callbacks[command_name]
+                callback = self.callbacks[callback_name]
+                rendered = callback(response)
+            else:
+                rendered = response
+        return rendered
 
     def send_command(self, command):
         # FIXME args include ", strip it
@@ -64,13 +78,4 @@ class Client:
         redis_commands = command.split(" ")
         logger.debug(f"[comamnd list] {redis_commands}")
         redis_resp = self.execute_command(*redis_commands)
-        # FIXME command name can not only split by " "
-        # command name include " "
-        # for key in variables:
-        #   key.starts_with command_*  then it is command
-        command_name = redis_commands[0]
-        if command_name in self.answer_callbacks:
-            rendered = self.answer_callbacks[command_name](redis_resp)
-        else:
-            rendered = redis_resp
         return redis_resp
