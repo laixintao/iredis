fd0da3b28f30f3e1deada655ab65be2c93c75feb
laixintao <laixintao1995@163.com>
2019-08-19 16:21:18 +0800

    fix testcase

--- tests/test_render_functions.py
+++ tests/test_render_functions.py
@@ -1,9 +1,14 @@
 from iredis import renders
 
 
+def strip_formatted_text(formatted_text):
+    return "".join(text[1] for text in formatted_text)
+
+
 def test_render_list_index():
     raw = ["hello", "world", "foo"]
     out = renders.render_list(raw)
+    out = strip_formatted_text(out)
     assert isinstance(out, str)
     assert "3)" in out
     assert "1)" in out
@@ -13,18 +18,21 @@ def test_render_list_index():
 def test_render_list_index_const_width():
     raw = ["hello"] * 100
     out = renders.render_list(raw)
+    out = strip_formatted_text(out)
     assert isinstance(out, str)
     assert "  1)" in out
     assert "\n100)" in out
 
     raw = ["hello"] * 1000
     out = renders.render_list(raw)
+    out = strip_formatted_text(out)
     assert "   1)" in out
     assert "\n 999)" in out
     assert "\n1000)" in out
 
     raw = ["hello"] * 10
     out = renders.render_list(raw)
+    out = strip_formatted_text(out)
     assert " 1)" in out
     assert "\n 9)" in out
     assert "\n10)" in out
