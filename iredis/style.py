25bfd4d1ab0b9ac032a8df4de559563a34c9eb58
laixintao <laixintao1995@163.com>
2019-08-19 14:52:32 +0800

    render list with color

--- iredis/style.py
+++ iredis/style.py
@@ -1,16 +1,15 @@
 
 from prompt_toolkit.styles import Style
 
+STYLE_DICT = {
+    # User input (default text).
+    "": "",
+    # Prompt.
+    "hostname": "",
+    "key": "#33aa33",
+    "integer": "#ff0000",
+    "trailing-input": "bg:#662222 #ffffff",
+    "password": "hidden",
+}
 
-STYLE = Style.from_dict(
-    {
-        # User input (default text).
-        "": "",
-        # Prompt.
-        "hostname": "",
-        "key": "#33aa33",
-        "integer": "#ff0000",
-        "trailing-input": "bg:#662222 #ffffff",
-        "password": "hidden",
-    }
-)
+STYLE = Style.from_dict(STYLE_DICT)
