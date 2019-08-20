c369ae938151060485d83c15ead8374453ae2eb6
laixintao <laixintao1995@163.com>
2019-08-20 10:10:16 +0800

    add ok render callback

--- iredis/style.py
+++ iredis/style.py
@@ -8,8 +8,12 @@ STYLE_DICT = {
     "hostname": "",
     "key": "#33aa33",
     "integer": "#ff0000",
-    "trailing-input": "bg:#662222 #ffffff",
+    "trailing-input": "bg:#ff0000 #000000",
     "password": "hidden",
+    # from bootstrap
+    # https://getbootstrap.com/docs/4.0/utilities/colors/
+    "success": "#A6E22E",  
+    "error": "#F92672",
 }
 
 STYLE = Style.from_dict(STYLE_DICT)
