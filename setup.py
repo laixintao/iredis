8eb39a08b2cf20c5ffd5d8ddd6b9cda8b9845fd0
laixintao <laixintao1995@163.com>
2019-08-20 16:51:08 +0800

    install click master from github
    open: https://github.com/laixintao/iredis/issues/81

--- setup.py
+++ setup.py
@@ -39,5 +39,4 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     entry_points={"console_scripts": ["iredis = iredis.entry:main"]},
-    dependency_links=["git+git://github.com/pallets/click@master#egg=click"],
 )
