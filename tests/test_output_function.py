48e4dd65c786db8dfa4a0edc326fc3acb746524c
laixintao <laixintao1995@163.com>
2019-08-21 10:36:14 +0800

    add chinese character test for output function

--- tests/test_output_function.py
+++ tests/test_output_function.py
@@ -9,3 +9,4 @@ def test_output_bytes():
     assert output.output_bytes(b'"hello\\world"') == '\\"hello\\\\world\\"'
 
     assert output.output_bytes(b"'\"") == "'\\\""
+    assert output.output_bytes("你好".encode()) == r"\xe4\xbd\xa0\xe5\xa5\xbd"
