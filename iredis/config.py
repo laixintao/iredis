76928c2e56e1c2cb8868033baeab3a589abaa423
laixintao <laixintao1995@163.com>
2019-08-21 09:22:20 +0800

    add comment for config

--- iredis/config.py
+++ iredis/config.py
@@ -2,6 +2,13 @@ class Config:
     """
     Global config, set once on start, then
     become readonly, never change again.
+
+    :param raw: weather write raw bytes to stdout without any
+        decoding.
+    :param decode: How to decode bytes response.(For display and
+        Completers)
+        default is None, means show literal bytes. But completers
+        will try use utf-8 decoding.
     """
 
     def __init__(self):
