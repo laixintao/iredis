7b90b1ac09b55ff153989c9cdab44a42e69b2784
laixintao <laixintao1995@163.com>
2019-08-23 23:01:43 +0800

    add since and complexity on bottom

--- iredis/style.py
+++ iredis/style.py
@@ -14,6 +14,23 @@ style = REDIS_TOKEN = {
     "double": "#bb6688",
 }
 
+GROUP = {
+    "group.cluster": "#E6DB74",
+    "group.connection": "#E6DB74",
+    "group.generic": "#E6DB74",
+    "group.geo": "#E6DB74",
+    "group.hash": "#E6DB74",
+    "group.hyperloglog": "#E6DB74",
+    "group.list": "#E6DB74",
+    "group.pubsub": "#E6DB74",
+    "group.server": "#E6DB74",
+    "group.set": "#E6DB74",
+    "group.sortedset": "#E6DB74",
+    "group.stram": "#E6DB74",
+    "group.string": "#E6DB74",
+    "group.transections": "#E6DB74",
+}
+
 
 STYLE_DICT = {
     # User input (default text).
@@ -31,6 +48,8 @@ STYLE_DICT = {
     "bottom-toolbar.on": "bg:#222222 #ffffff",
     "bottom-toolbar.off": "bg:#222222 #888888",
     "bottom-toolbar.loaded": "bg:#222222 #44aa44",
+    "bottom-toolbar.since": "bg:#222222 #bc7a00",
+    "bottom-toolbar.complexity": "bg:#222222 #666666",
     # completion
     "completion-menu.completion.current": "bg:#ffffff #000000",
     "completion-menu.completion": "bg:#008888 #ffffff",
