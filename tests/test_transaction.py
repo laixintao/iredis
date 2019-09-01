aad4857846ee6b9e0bf5c6e9b1c526350b641311
laixintao <laixintao1995@163.com>
2019-09-01 13:17:47 +0800

    add testcase for transaction error

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,3 +1,18 @@
-def test_trasaction_rprompt(onetime_process):
-    onetime_process.sendline("multi")
-    onetime_process.expect("
+def test_trasaction_rprompt(local_process):
+    local_process.sendline("multi")
+    local_process.expect("OK")
+    local_process.expect("transaction")
+
+    local_process.sendline("get foo")
+    local_process.expect("QUEUED")
+
+    local_process.sendline("EXEC")
+
+
+def test_trasaction_syntax_error(local_process):
+    local_process.sendline("multi")
+    local_process.sendline("get foo 1")
+    local_process.expect("wrong number of arguments for 'get' command")
+
+    local_process.sendline("EXEC")
+    local_process.expect("Transaction discarded because of previous errors.")
