1507d763b96183a83a2321d7d915a214e1f8b9d6
laixintao <laixintao1995@163.com>
2019-09-02 19:42:08 +0800

    command mget

--- tests/test_render_functions.py
+++ tests/test_render_functions.py
@@ -5,12 +5,12 @@ def strip_formatted_text(formatted_text):
     return "".join(text[1] for text in formatted_text)
 
 
-def test_render_list_index():
+def test__render_list_index():
     from iredis.config import config
 
     config.raw = False
     raw = ["hello", "world", "foo"]
-    out = renders.render_list([item.encode() for item in raw], raw)
+    out = renders._render_list([item.encode() for item in raw], raw)
     out = strip_formatted_text(out)
     assert isinstance(out, str)
     assert "3)" in out
@@ -18,38 +18,38 @@ def test_render_list_index():
     assert "4)" not in out
 
 
-def test_render_list_index_const_width():
+def test__render_list_index_const_width():
     from iredis.config import config
 
     config.raw = False
     raw = ["hello"] * 100
-    out = renders.render_list([item.encode() for item in raw], raw)
+    out = renders._render_list([item.encode() for item in raw], raw)
     out = strip_formatted_text(out)
     assert isinstance(out, str)
     assert "  1)" in out
     assert "\n100)" in out
 
     raw = ["hello"] * 1000
-    out = renders.render_list([item.encode() for item in raw], raw)
+    out = renders._render_list([item.encode() for item in raw], raw)
     out = strip_formatted_text(out)
     assert "   1)" in out
     assert "\n 999)" in out
     assert "\n1000)" in out
 
     raw = ["hello"] * 10
-    out = renders.render_list([item.encode() for item in raw], raw)
+    out = renders._render_list([item.encode() for item in raw], raw)
     out = strip_formatted_text(out)
     assert " 1)" in out
     assert "\n 9)" in out
     assert "\n10)" in out
 
 
-def test_render_list_while_config_raw():
+def test__render_list_while_config_raw():
     from iredis.config import config
 
     config.raw = True
     raw = ["hello", "world", "foo"]
-    out = renders.render_list([item.encode() for item in raw], raw)
+    out = renders._render_list([item.encode() for item in raw], raw)
     assert b"hello\nworld\nfoo" == out
 
 
