ca1e2f8390d8d75e79d4cec432aa7b9f0eb52447
laixintao <laixintao1995@163.com>
2019-09-04 21:10:04 +0800

    testcase for SDIFFSTORE

--- tests/command_parse/test_set_parse.py
+++ tests/command_parse/test_set_parse.py
@@ -3,3 +3,23 @@ def test_sadd(judge_command):
         "SADD foo m1 m2 m3",
         {"command_key_members": "SADD", "key": "foo", "members": "m1 m2 m3"},
     )
+    judge_command(
+        "SADD foo m1", {"command_key_members": "SADD", "key": "foo", "members": "m1"}
+    )
+    judge_command("SADD foo", None)
+
+
+def test_sdiffstore(judge_command):
+    judge_command(
+        "SDIFFSTORE foo m1 m2 m3",
+        {
+            "command_destination_keys": "SDIFFSTORE",
+            "destination": "foo",
+            "keys": "m1 m2 m3",
+        },
+    )
+    judge_command(
+        "SDIFFSTORE foo m1",
+        {"command_destination_keys": "SDIFFSTORE", "destination": "foo", "keys": "m1"},
+    )
+    judge_command("SDIFFSTORE foo", None)
