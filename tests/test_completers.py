52de20416413bcd9665c6620483bcb8b160d98a0
laixintao <laixintao1995@163.com>
2019-09-05 20:35:10 +0800

    add testcase for newbie mode

--- tests/test_completers.py
+++ tests/test_completers.py
@@ -1,4 +1,10 @@
+from unittest.mock import MagicMock
+from prompt_toolkit.completion import Completion
+from prompt_toolkit.formatted_text import FormattedText
+
 from iredis.completers import LatestUsedFirstWordCompleter
+from iredis.config import config
+from .conftest import prompt_session
 
 
 def test_LUF_completer_touch():
@@ -20,3 +26,62 @@ def test_LUF_completer_touch_words():
 
     c.touch_words(["one", "two"])
     assert c.words == ["two", "one", "bar"]
+
+
+def test_newbie_mode_complete_without_meta_dict():
+    config.newbie_mode = False
+    session = prompt_session()
+    fake_document = MagicMock()
+    fake_document.text_before_cursor = "get"
+    completions = list(session.completer.get_completions(fake_document, None))
+    assert completions == [
+        Completion(
+            text="get",
+            start_position=-3,
+            display=FormattedText([("", "get")]),
+            display_meta=FormattedText([("", "")]),
+        ),
+        Completion(
+            text="getset",
+            start_position=-3,
+            display=FormattedText([("", "getset")]),
+            display_meta=FormattedText([("", "")]),
+        ),
+        Completion(
+            text="getrange",
+            start_position=-3,
+            display=FormattedText([("", "getrange")]),
+            display_meta=FormattedText([("", "")]),
+        ),
+        Completion(
+            text="getbit",
+            start_position=-3,
+            display=FormattedText([("", "getbit")]),
+            display_meta=FormattedText([("", "")]),
+        ),
+    ]
+
+
+def test_newbie_mode_complete_with_meta_dict():
+    config.newbie_mode = True
+    session = prompt_session()
+    fake_document = MagicMock()
+    fake_document.text_before_cursor = "get"
+    completions = list(session.completer.get_completions(fake_document, None))
+
+    assert completions[:2] == [
+        Completion(
+            text="get",
+            start_position=-3,
+            display=FormattedText([("", "get")]),
+            display_meta=FormattedText([("", "Get the value of a key")]),
+        ),
+        Completion(
+            text="getset",
+            start_position=-3,
+            display=FormattedText([("", "getset")]),
+            display_meta=FormattedText(
+                [("", "Set the string value of a key and return its old value")]
+            ),
+        ),
+    ]
