153304b966de43f74cd9579a0805fde3db7f7c40
laixintao <laixintao1995@163.com>
2019-09-10 11:58:52 +0800

    testcase for bzpopmax

--- tests/command_parse/test_sorted_set_commandz_pass.py
+++ tests/command_parse/test_sorted_set_commandz_pass.py
@@ -3,3 +3,14 @@ def test_zcount(judge_command):
         "zcount foo -10 0",
         {"command_key_min_max": "zcount", "key": "foo", "min": "-10", "max": "0"},
     )
+
+
+def test_bzpopmax(judge_command):
+    judge_command(
+        "bzpopmax set set2 set3 4",
+        {"command_keys_timeout": "bzpopmax", "keys": "set set2 set3", "timeout": "4"},
+    )
+    judge_command(
+        "bzpopmin set   4",
+        {"command_keys_timeout": "bzpopmin", "keys": "set", "timeout": "4"},
+    )
