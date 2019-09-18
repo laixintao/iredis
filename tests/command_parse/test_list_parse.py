19df151dc5caecdf948f6135d92f69fb5e72a2f6
laixintao <laixintao1995@163.com>
2019-09-19 01:36:53 +0800

    LSET

--- tests/command_parse/test_list_parse.py
+++ tests/command_parse/test_list_parse.py
@@ -23,3 +23,24 @@ def test_lindex(judge_command):
         {"command_key_position": "LINDEX", "key": "list1", "position": "-10"},
     )
     judge_command("LINDEX list1 1.1", None)
+
+
+def test_lset(judge_command):
+    judge_command(
+        "LSET list1 10 newbie",
+        {
+            "command_key_position_value": "LSET",
+            "key": "list1",
+            "position": "10",
+            "value": "newbie",
+        },
+    )
+    judge_command(
+        "LSET list1 -1 newbie",
+        {
+            "command_key_position_value": "LSET",
+            "key": "list1",
+            "position": "-1",
+            "value": "newbie",
+        },
+    )
