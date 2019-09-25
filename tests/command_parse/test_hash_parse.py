552b1438ffcc63ab3fa28eb9f45796f398902161
laixintao <laixintao1995@163.com>
2019-09-25 17:40:43 +0800

    test for hmset

--- tests/command_parse/test_hash_parse.py
+++ tests/command_parse/test_hash_parse.py
@@ -6,3 +6,24 @@ def test_hdel(judge_command):
         "HDEL foo bar hello world",
         {"command_key_fields": "HDEL", "key": "foo", "fields": "bar hello world"},
     )
+
+
+def test_hmset(judge_command):
+    judge_command(
+        "HMSET foo bar hello-world",
+        {
+            "command_key_fieldvalues": "HMSET",
+            "key": "foo",
+            "field": "bar",
+            "value": "hello-world",
+        },
+    )
+    judge_command(
+        "HMSET foo bar hello-world key2 value2",
+        {
+            "command_key_fieldvalues": "HMSET",
+            "key": "foo",
+            "field": "key2",
+            "value": "value2",
+        },
+    )
