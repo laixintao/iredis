d21b66e72115dbf98027a58a4ea114c1102087db
laixintao <laixintao1995@163.com>
2019-10-31 20:43:00 -0600

    Case insensetive tokens

--- iredis/redis_lexer.py
+++ iredis/redis_lexer.py
@@ -1,20 +1,22 @@
 from pygments.lexer import RegexLexer
-from pygments.token import Keyword, Name
+from pygments.token import Keyword, Name, Whitespace
 
 
 class RedisLexer(RegexLexer):
     name = "redis lexer"
 
     tokens = {
-        "root": [
-            (r"GET", Keyword, "get"),
-        ],
-        "get": [
-            (r"\w+", Name, "#pop"),
-        ],
+        "root": [(r"\s+", Whitespace), (r"(?i)GET", Keyword, "get")],
+        "get": [(r"\s+", Whitespace), (r"\w+", Name, "#pop")],
     }
 
 
 r = RedisLexer()
 result = list(r.get_tokens_unprocessed("GET foo"))
+print(result)
+
+result = list(r.get_tokens_unprocessed("get foo"))
+print(result)
+
+result = list(r.get_tokens_unprocessed(" get foo"))
 print(result)
