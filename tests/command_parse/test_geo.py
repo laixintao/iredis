c6c0fbb9472f656956208ee842e7afe1290414d0
laixintao <laixintao1995@163.com>
2019-12-25 22:15:47 +0800

    add testcase

--- tests/command_parse/test_geo.py
+++ tests/command_parse/test_geo.py
@@ -9,3 +9,31 @@ def test_geoadd(judge_command):
             "member": '"Catania"',
         },
     )
+
+
+def test_georadiusbymember(judge_command):
+    judge_command(
+        "GEORADIUSBYMEMBER Sicily Agrigento 100 km",
+        {
+            "command_georadiusbymember": "GEORADIUSBYMEMBER",
+            "key": "Sicily",
+            "member": "Agrigento",
+            "float": "100",
+            "distunit": "km",
+        },
+    )
+
+
+def test_georadius(judge_command):
+    judge_command(
+        "GEORADIUS Sicily 15 37 200 km WITHDIST WITHCOORD ",
+        {
+            "command_radius": "GEORADIUS",
+            "key": "Sicily",
+            "longitude": "15",
+            "latitude": "37",
+            "float": "200",
+            "distunit": "km",
+            "geochoice": "WITHCOORD",
+        },
+    )
