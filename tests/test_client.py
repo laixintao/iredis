bf3db1f0fd7a888b5aa4c13c63bffd372bf59014
laixintao <laixintao1995@163.com>
2020-01-11 20:14:17 +0800

    add tests for rainbow prompt

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,9 +1,13 @@
 import pytest
 from unittest.mock import MagicMock
+
+from prompt_toolkit.contrib.regular_languages.completion import GrammarCompleter
+
 from iredis.client import Client
 from iredis.completers import completer_mapping
-from prompt_toolkit.contrib.regular_languages.completion import GrammarCompleter
 from iredis.redis_grammar import get_command_grammar
+from iredis.entry import Rainbow, prompt_message
+from iredis.config import config
 
 
 @pytest.mark.parametrize(
@@ -40,7 +44,6 @@ def test_patch_completer():
 
 
 def test_get_server_verison_after_client():
-    from iredis.config import config
 
     Client("127.0.0.1", "6379", None)
     assert config.version.startswith("5.")
@@ -51,7 +54,6 @@ def test_get_server_verison_after_client():
 
 
 def test_do_help():
-    from iredis.config import config
 
     client = Client("127.0.0.1", "6379", None)
     config.version = "5.0.0"
@@ -60,3 +62,36 @@ def test_do_help():
     config.version = "2.0.0"
     resp = client.do_help("cluster", "addslots")
     assert resp[10] == ("", "3.0.0 (Not avaiable on your redis-server: 2.0.0)")
+
+
+def test_rainbow_iterator():
+    "test color infinite iterator"
+    original_color = Rainbow.color
+    Rainbow.color = list(range(0, 3))
+    assert list(zip(range(10), Rainbow())) == [
+        (0, 0),
+        (1, 1),
+        (2, 2),
+        (3, 1),
+        (4, 0),
+        (5, 1),
+        (6, 2),
+        (7, 1),
+        (8, 0),
+        (9, 1),
+    ]
+    Rainbow.color = original_color
+
+
+def test_prompt_message(iredis_client):
+    original_rainbow = config.rainbow
+    config.rainbow = False
+    assert prompt_message(iredis_client) == "127.0.0.1:6379> "
+
+    config.rainbow = True
+    assert prompt_message(iredis_client)[:3] == [
+        ("#cc2244", "1"),
+        ("#bb4444", "2"),
+        ("#996644", "7"),
+    ]
+    config.rainbow = original_rainbow
