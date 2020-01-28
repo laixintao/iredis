8e9f9a169a2d6539498b06db6aa7c05b003d2c56
laixintao <laixintao1995@163.com>
2020-01-28 19:07:50 +0800

    move a left cli test to cli_tests/

--- tests/unittests/test_entry.py
+++ tests/unittests/test_entry.py
@@ -1,10 +1,42 @@
-from iredis.entry import write_result
+import pytest
+from unittest.mock import patch
+
+from iredis.entry import gather_args
 
 
-def test_when_using_decode(config, capfd):
-    config.decode = "utf-8"
-    config.raw = True
-    write_result("你好")
+@pytest.mark.parametrize(
+    "is_tty,raw_arg_is_raw,final_config_is_raw",
+    [
+        (True, None, False),
+        (True, True, True),
+        (True, False, False),
+        (False, None, True),
+        (False, True, True),
+        (False, False, True),  # not tty
+    ],
+)
+def test_command_entry_tty(is_tty, raw_arg_is_raw, final_config_is_raw, config):
+    # is tty + raw -> raw
+    with patch("sys.stdout.isatty") as patch_tty:
 
-    out, err = capfd.readouterr()
-    assert out == "你好\n"
+        patch_tty.return_value = is_tty
+        if raw_arg_is_raw is None:
+            call = ["iredis"]
+        elif raw_arg_is_raw is True:
+            call = ["iredis", "--raw"]
+        elif raw_arg_is_raw is False:
+            call = ["iredis", "--no-raw"]
+        else:
+            raise Exception()
+        gather_args.main(call, standalone_mode=False)
+        assert config.raw == final_config_is_raw
+
+
+def test_command_with_decode_utf_8():
+    from iredis.config import config
+
+    gather_args.main(["iredis", "--decode", "utf-8"], standalone_mode=False)
+    assert config.decode == "utf-8"
+
+    gather_args.main(["iredis"], standalone_mode=False)
+    assert config.decode == ""
