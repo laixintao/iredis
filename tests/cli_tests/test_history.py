b33fc55cecda8236c62c150c9c97ab3c168a2f1b
laixintao <laixintao1995@163.com>
2020-02-28 08:58:22 +0800

    tests for ingore auth command in history.

---
+++ tests/cli_tests/test_history.py
@@ -0,0 +1,11 @@
+def test_history_not_log_auth(cli):
+    cli.sendline("AUTH 123")
+    cli.expect(["Client sent AUTH, but no password is set", "127.0.0.1"])
+    cli.sendline("set foo bar")
+    cli.expect("OK")
+
+    with open("~/.iredis_history", "r") as history_file:
+         content = history_file.read()
+
+    assert "set foo bar" in content
+    assert "AUTH" not in content
