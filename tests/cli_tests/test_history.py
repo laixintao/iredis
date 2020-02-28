6395ece8f4815e4e36edf6f284d1b460eb6ef5e6
laixintao <laixintao1995@163.com>
2020-02-28 09:09:34 +0800

    tests for history config.

--- tests/cli_tests/test_history.py
+++ tests/cli_tests/test_history.py
@@ -1,4 +1,7 @@
 import os
+import pexpect
+from pathlib import Path
+from textwrap import dedent
 
 
 def test_history_not_log_auth(cli):
@@ -12,3 +15,28 @@ def test_history_not_log_auth(cli):
 
     assert "set foo bar" in content
     assert "AUTH" not in content
+
+
+def test_history_create_and_writing_with_config():
+    config_content = dedent(
+        """
+        [main]
+        history_location = /tmp/iredis_history.txt
+        """
+    )
+    with open("/tmp/iredisrc", "w+") as etc_config:
+        etc_config.write(config_content)
+
+    cli = pexpect.spawn("iredis -n 15 --iredisrc /tmp/iredisrc", timeout=2)
+    cli.expect("127.0.0.1")
+    cli.sendline("set hello world")
+    cli.expect("OK")
+    cli.close()
+
+    log = Path("/tmp/iredis_history.txt")
+    assert log.exists()
+
+    with open(log, "r") as logfile:
+        content = logfile.read()
+
+    assert "set hello world" in content
