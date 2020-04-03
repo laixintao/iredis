8fbcaccba2885f94b91679e5d33bbf7d65b5949e
laixintao <laixintaoo@gmail.com>
2020-04-03 23:50:00 +0800

    latency graph using upper case for const completion.

--- iredis/redis_grammar.py
+++ iredis/redis_grammar.py
@@ -79,23 +79,24 @@ CONST = {
     "overflow": "OVERFLOW",
     "overflow_option": "WRAP SAT FAIL",
     "version": "VERSION",
+
     "graphevent": (
-        "active-defrag-cycle "
-        "aof-fsync-always "
-        "aof-stat "
-        "aof-rewrite-diff-write "
-        "aof-rename "
-        "aof-write "
-        "aof-write-active-child "
-        "aof-write-alone "
-        "aof-write-pending-fsync "
-        "command "
-        "expire-cycle "
-        "eviction-cycle "
-        "eviction-del "
-        "fast-command "
-        "fork "
-        "rdb-unlink-temp-file"
+        "ACTIVE-DEFRAG-CYCLE "
+        "AOF-FSYNC-ALWAYS "
+        "AOF-STAT "
+        "AOF-REWRITE-DIFF-WRITE "
+        "AOF-RENAME "
+        "AOF-WRITE "
+        "AOF-WRITE-ACTIVE-CHILD "
+        "AOF-WRITE-ALONE "
+        "AOF-WRITE-PENDING-FSYNC "
+        "COMMAND "
+        "EXPIRE-CYCLE "
+        "EVICTION-CYCLE "
+        "EVICTION-DEL "
+        "FAST-COMMAND "
+        "FORK "
+        "RDB-UNLINK-TEMP-FILE"
     ),
 }
 
